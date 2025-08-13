ce3f69a138e0acd74ce1a8e4b755b60643cf1e93
Gabriela Araujo <gabyara237@gmail.com>
2025-08-13 00:47:15 -0700

    feat: Initial implementation of database models

---
+++ mastermind-api/app/models.py
@@ -0,0 +1,49 @@
+from typing import Optional, List
+from sqlmodel import Field, SQLModel, Relationship
+
+class Player(SQLModel, table= True):
+    """
+     Model for players.
+     Id is the primary key
+    """
+    id: Optional[int]= Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    score: int = Field(default=0)
+
+    # One to many relationship with GameSession
+    game_sessions: List["GameSession"]= Relationship(back_populates="player")
+
+class GameSession(SQLModel, tabla= True):
+    """
+     Model for game sessions.
+     Represents a Mastermind game
+    """
+    id: Optional[int] = Field(default=None, primary_key=True)
+    secret_number: str
+    difficulty_level: int
+    attempts_left: int
+    is_active: bool = Field(default=True)
+
+    # Foreign key to relate to Player
+    player_id: Optional[int]= Field(default=None, foreign_key="player.id")
+
+    # Many to one relationship with Player
+    player: Optional[Player] = Relationship(back_populates="game_sessions") 
+    
+    # One to many relationship with GameAttempt
+    attempts: List["GameAttempt"] = Relationship(back_populates="game_session")
+
+class GameAttempt(SQLModel, table= True):
+    """
+     Model for a player's attempts in a game session.
+    """
+    id: Optional[int] = Field(default=None, primary_key=True)
+    guessed_number: str
+    correct_numbers: int
+    correct_position: int
+
+    # Foreign key to relate GameSession
+    game_session_id: Optional[int] = Field(default=None,foreign_key="game_session.id")
+    
+    # Many to one relationship with GameSession
+    game_session: Optional["GameSession"] = Relationship(back_populates="attempts")
