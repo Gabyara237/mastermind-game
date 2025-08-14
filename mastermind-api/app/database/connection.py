2eb41c0d8da8b0cd8a617d679386c7e279e6cc9b
Gabriela Araujo <gabyara237@gmail.com>
2025-08-13 22:33:17 -0700

    docs: Add documentation

--- mastermind-api/app/database/connection.py
+++ mastermind-api/app/database/connection.py
@@ -2,7 +2,9 @@ from sqlmodel import create_engine, Session, SQLModel
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    
+    """
+     Database configuration
+    """
     database_url: str ="sqlite: ///./mastermind.db"
 
     class Config:
@@ -14,8 +16,14 @@ settings = Settings()
 engine= create_engine(settings.database_url, echo=True, connect_args={"check_same_thread": False})
 
 def create_db_and_tables():
+    """
+     Create the database tables if they do not exist
+    """
     SQLModel.metadata.create_all(engine)
 
 def get_session():
+    """
+     Dependency function to obtain a database session.
+    """
     with Session(engine) as session:
         yield session
