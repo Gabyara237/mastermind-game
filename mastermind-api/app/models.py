5ba3b92b746ee1720e0e37a756139d48df90b90f
Gabriela Araujo <gabyara237@gmail.com>
2025-08-17 21:36:27 -0700

    feat: Implement user authentication with JWT and password hashing

--- mastermind-api/app/models.py
+++ mastermind-api/app/models.py
@@ -1,20 +1,47 @@
+from datetime import datetime, timezone
 from typing import Optional, List
 from sqlmodel import Field, SQLModel, Relationship
 
+
+class User(SQLModel, table = True):
+    """
+        System User- Authentication only
+    """
+    __tablename__="users"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    hashed_password: str
+    created_at: datetime = Field(default_factory= lambda:datetime.now(timezone.utc))
+
+    player: Optional["Player"] = Relationship(
+        back_populates = "user",
+        sa_relationship_kwargs={"uselist": False}
+    )
+
 class Player(SQLModel, table= True):
     """
-     Model for players.
-     Id is the primary key
+        Game profile. Model for players.
     """
     __tablename__="player"
 
     id: Optional[int]= Field(default=None, primary_key=True)
-    name: str = Field(index=True)
+    user_id: int = Field(foreign_key="users.id", unique= True)
+
+
     score: int = Field(default=0)
     last_attempt_score: Optional[int] = Field(default=None)
 
+    #Relationships
+    user: User = Relationship(back_populates="player")
     # One to many relationship with GameSession
     game_sessions: List["GameSession"]= Relationship(back_populates="player")
+
+    @property
+    def display_name(self) -> str:
+        return self.user.username
+
 
 class GameSession(SQLModel, table= True):
     """
