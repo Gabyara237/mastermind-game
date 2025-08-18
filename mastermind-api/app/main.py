819bac1ba023f04a7f1b9794a5b075dbf1004fa1
Gabriela Araujo <gabyara237@gmail.com>
2025-08-17 21:59:30 -0700

    feat: configure main app with authentication and CORS

--- mastermind-api/app/main.py
+++ mastermind-api/app/main.py
@@ -1,19 +1,45 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 from app.database.connection import create_db_and_tables
+
 from app.routes.game_router import router as game_router
+from app.routes.auth_routes import router as auth_router
 
-app = FastAPI(title ="Mastermind API")
 
-app.include_router(game_router, prefix="/api/v1")
 
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
     print("Database and tables created successfully")
+    yield
+
+
+
+app = FastAPI(
+    title ="Mastermind API",
+    lifespan= lifespan    
+)
+
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
+# Routers
+app.include_router(auth_router,prefix="/api/v1")
+app.include_router(game_router, prefix="/api/v1")
+
 
 @app.get("/")
 def read_root():
     return{"message": "Welcome!"}
+
 
 if __name__ == "__main__":
     import uvicorn
