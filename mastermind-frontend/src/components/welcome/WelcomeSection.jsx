740de23e477a5062a0284a72d4266cc88156cc0d
Gabriela Araujo <gabyara237@gmail.com>
2025-08-19 05:38:26 -0700

    feat: Add WelcomeSection component

---
+++ mastermind-frontend/src/components/welcome/WelcomeSection.jsx
@@ -0,0 +1,12 @@
+const WelcomeSection = () =>{
+    return(
+        <>
+            <h2>
+                Welcome to <br/>
+                <span className="title-welcome-section">Mastermind</span>
+            </h2>
+        </>
+    )
+}
+
+export default WelcomeSection;
